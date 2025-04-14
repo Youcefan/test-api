adab4b41ce6f7c3c6423278398557f58f70f5c1c
Youcefan <youceftaleb90@gmail.com>
2025-04-14 11:53:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const path = require("path");
 const mongoose = require("mongoose");
 const usersRouter = require("./Routes/usersRoutes");
